f5e10f87e1c1a2dce18bc5bef96aeaa2ab816889
Vitaliy Roy <vitaliybrandet@gmail.com>
2023-05-26 11:39:15 +0300

    Добавьте файлы проекта.

---
+++ lr31/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace lr31
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.processListView = new System.Windows.Forms.ListView();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.viewProcessInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewThreadsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewModulesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // processListView
+            // 
+            this.processListView.HideSelection = false;
+            this.processListView.Location = new System.Drawing.Point(12, 12);
+            this.processListView.Name = "processListView";
+            this.processListView.Size = new System.Drawing.Size(450, 426);
+            this.processListView.TabIndex = 0;
+            this.processListView.UseCompatibleStateImageBehavior = false;
+            this.processListView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.processListView_MouseClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewProcessInfoToolStripMenuItem,
+            this.stopProcessToolStripMenuItem,
+            this.refreshToolStripMenuItem,
+            this.exportToFileToolStripMenuItem,
+            this.viewThreadsToolStripMenuItem,
+            this.viewModulesToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(167, 136);
+            // 
+            // viewProcessInfoToolStripMenuItem
+            // 
+            this.viewProcessInfoToolStripMenuItem.Name = "viewProcessInfoToolStripMenuItem";
+            this.viewProcessInfoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewProcessInfoToolStripMenuItem.Text = "View Process Info";
+            this.viewProcessInfoToolStripMenuItem.Click += new System.EventHandler(this.viewProcessInfoToolStripMenuItem_Click);
+            // 
+            // stopProcessToolStripMenuItem
+            // 
+            this.stopProcessToolStripMenuItem.Name = "stopProcessToolStripMenuItem";
+            this.stopProcessToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.stopProcessToolStripMenuItem.Text = "Stop Process";
+            this.stopProcessToolStripMenuItem.Click += new System.EventHandler(this.stopProcessToolStripMenuItem_Click);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // exportToFileToolStripMenuItem
+            // 
+            this.exportToFileToolStripMenuItem.Name = "exportToFileToolStripMenuItem";
+            this.exportToFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportToFileToolStripMenuItem.Text = "Export to File";
+            this.exportToFileToolStripMenuItem.Click += new System.EventHandler(this.exportToFileToolStripMenuItem_Click);
+            // 
+            // viewThreadsToolStripMenuItem
+            // 
+            this.viewThreadsToolStripMenuItem.Name = "viewThreadsToolStripMenuItem";
+            this.viewThreadsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewThreadsToolStripMenuItem.Text = "View Threads";
+            this.viewThreadsToolStripMenuItem.Click += new System.EventHandler(this.viewThreadsToolStripMenuItem_Click);
+            // 
+            // viewModulesToolStripMenuItem
+            // 
+            this.viewModulesToolStripMenuItem.Name = "viewModulesToolStripMenuItem";
+            this.viewModulesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewModulesToolStripMenuItem.Text = "View Modules";
+            this.viewModulesToolStripMenuItem.Click += new System.EventHandler(this.viewModulesToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 450);
+            this.Controls.Add(this.processListView);
+            this.Name = "Form1";
+            this.Text = "Лабораторна робота 31";
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView processListView;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem viewProcessInfoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopProcessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewThreadsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewModulesToolStripMenuItem;
+    }
+}
+
---
+++ lr31/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace lr31
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            PopulateProcessList();
+        }
+        private void PopulateProcessList()
+        {
+            processListView.Items.Clear();
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.SubItems.Add(process.Id.ToString());
+                processListView.Items.Add(item);
+            }
+        }
+        private void viewProcessInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (processListView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = processListView.SelectedItems[0];
+                string processName = selectedItem.SubItems[0].Text;
+                int processId = int.Parse(selectedItem.SubItems[1].Text);
+
+                Process process = Process.GetProcessById(processId);
+                string message = $"Process Name: {processName}\nProcess ID: {processId}\n";
+                message += $"Start Time: {process.StartTime}\nTotal Processor Time: {process.TotalProcessorTime}\n";
+                message += $"Physical Memory Usage: {process.WorkingSet64} bytes";
+
+                MessageBox.Show(message, "Process Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void processListView_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (processListView.FocusedItem.Bounds.Contains(e.Location))
+                {
+                    contextMenuStrip.Show(Cursor.Position);
+                }
+            }
+        }
+
+        private void stopProcessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (processListView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = processListView.SelectedItems[0];
+                int processId = int.Parse(selectedItem.SubItems[1].Text);
+
+                try
+                {
+                    Process process = Process.GetProcessById(processId);
+                    process.Kill();
+                    process.WaitForExit();
+
+                    MessageBox.Show("Process stopped successfully.", "Process Stopped", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred while stopping the process: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                PopulateProcessList();
+            }
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PopulateProcessList();
+        }
+
+        private void exportToFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            saveFileDialog.Title = "Export Process List";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        foreach (ListViewItem item in processListView.Items)
+                        {
+                            writer.WriteLine($"{item.SubItems[0].Text}\t{item.SubItems[1].Text}");
+                        }
+                    }
+
+                    MessageBox.Show("Process list exported successfully.", "Export Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred while exporting the process list: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void viewThreadsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (processListView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = processListView.SelectedItems[0];
+                int processId = int.Parse(selectedItem.SubItems[1].Text);
+
+                Process process = Process.GetProcessById(processId);
+                string message = $"Threads of Process: {process.ProcessName} (ID: {processId})\n\n";
+
+                foreach (ProcessThread thread in process.Threads)
+                {
+                    message += $"Thread ID: {thread.Id}\tThread State: {thread.ThreadState}\n";
+                }
+
+                MessageBox.Show(message, "Process Threads", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void viewModulesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (processListView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = processListView.SelectedItems[0];
+                int processId = int.Parse(selectedItem.SubItems[1].Text);
+
+                Process process = Process.GetProcessById(processId);
+                string message = $"Modules of Process: {process.ProcessName} (ID: {processId})\n\n";
+
+                foreach (ProcessModule module in process.Modules)
+                {
+                    message += $"Module Name: {module.ModuleName}\nModule File Name: {module.FileName}\n\n";
+                }
+
+                MessageBox.Show(message, "Process Modules", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
